69f129d46efd9d9179b1392c52afc2c0975d7488
Gaoce <gaocegege@hotmail.com>
2015-11-07 17:09:35 +0800

    update: socketio

--- index.js
+++ index.js
@@ -6,7 +6,14 @@ app.get('/', function(req, res){
 	res.send('<h1>Yeah! I am healthy!</h1>');
 });
 
+io.on('connection', function (socket) {
+  var addedUser = false;
 
+  // when the client emits 'new message', this listens and executes
+  socket.on('new message', function (data) {
+    console.log("qiu dai");
+  });
+});
 
 http.listen(3000, function(){
 	console.log('listening on *:3000');
